d6af6f078c5484f878fa903e17fff655e9d1fe31
fbxiang <fxiang2@illinois.edu>
2017-10-02 14:31:02 -0500

    better mouse

--- frontend/game.ts
+++ frontend/game.ts
@@ -1,6 +1,7 @@
 import { Vector3, Color4 } from 'babylonjs';
 import * as Babylon from 'babylonjs';
 import { Debug } from './debug';
+import * as _ from 'lodash';
 
 Debug.forwardVector = true;
 
@@ -51,7 +52,6 @@ export class Game {
     this._camera.panningSensibility = 0;
     const mouse = <Babylon.ArcRotateCameraPointersInput>this.camera.inputs.attached.pointers;
     mouse.buttons = [1];
-    console.log(camera.inputs.attached);
   }
 
   private initKeys() {
@@ -68,23 +68,44 @@ export class Game {
       e.preventDefault();
     })
 
-    this._canvas.addEventListener('click', e => {
+    this._canvas.addEventListener('mousedown', e => {
+      this.keydown['mouse'] = true;
       const pickResult = this.scene.pick(e.x, e.y);
       if (pickResult.hit) {
         this.player.target = pickResult.pickedPoint;
       }
+    })
+
+    this._canvas.addEventListener('mouseup', e => {
+      this.keydown['mouse'] = false;
+    })
+
+    this._canvas.addEventListener('mousemove', e => {
+      this.Input.mouse.x = e.x;
+      this.Input.mouse.y = e.y;
     })
   }
 
   Input = {
     keydown: (key: string) => {
       return this.keydown[key];
-    }
+    },
+    mouse: { x: -1, y: -1 }
   }
 
   render() {
     this.scene.render();
+
+    if (this.keydown['mouse']) {
+      const pickResult = this.scene.pick(this.Input.mouse.x, this.Input.mouse.y);
+      if (pickResult.hit) {
+        this.player.target = pickResult.pickedPoint;
+      }
+    }
+
     this.camera.target = this.player.position;
+    this.camera.radius = _.clamp(this.camera.radius, 15, 30);
+    this.camera.radius
     const deltaTime = this.scene.getLastFrameDuration() / 1000;
     this.player.update(deltaTime);
   }
@@ -104,7 +125,7 @@ class Entity {
     if (Debug.forwardVector) {
       if (!this._forwardMesh) {
         this._forwardMesh = Babylon.Mesh.CreateLines(`${name}: forward`, [Vector3.Zero(), Vector3.Forward()],
-                                                     this.game.scene, false);
+          this.game.scene, false);
         this._forwardMesh.parent = this._mesh;
       }
     } else {
@@ -135,6 +156,19 @@ class Player extends Entity {
   get target() { return this._target; }
   get position() { return this._mesh.position }
 
+  constructor(game: Game) {
+    super('player', game);
+    this._mesh = Babylon.Mesh.CreateBox('player', 1, game.scene);
+    this._mesh.position.y = 1;
+
+    const playerMaterial = new Babylon.StandardMaterial('player', game.scene);
+    playerMaterial.diffuseColor = Babylon.Color3.Green();
+    this._mesh.material = playerMaterial;
+
+    this.initTargetMesh();
+
+    this.initPhysics();
+  }
 
   update(deltaTime: number) {
     super.update(deltaTime);
@@ -142,7 +176,7 @@ class Player extends Entity {
       const dx = this.target.x - this.position.x;
       const dz = this.target.z - this.position.z;
 
-      const angle = dz > 0 ? Math.atan(dx/dz) : Math.atan(dx/dz)+Math.PI;
+      const angle = dz > 0 ? Math.atan(dx / dz) : Math.atan(dx / dz) + Math.PI;
 
       this.mesh.rotationQuaternion = Babylon.Quaternion.RotationAxis(
         Babylon.Axis.Y, angle);
@@ -156,23 +190,12 @@ class Player extends Entity {
     }
   }
 
-  constructor(game: Game) {
-    super('player', game);
-    this._mesh = Babylon.Mesh.CreateBox('player', 1, game.scene);
-
-    const playerMaterial = new Babylon.StandardMaterial('player', game.scene);
-    playerMaterial.diffuseColor = Babylon.Color3.Green();
-    this._mesh.material = playerMaterial;
-
-    this.initTargetMesh();
-
-    this.initPhysics();
-  }
-
   // TODO: Make the particle system work!!!
   initTargetMesh() {
     this._targetMesh = Babylon.Mesh.CreateSphere('player_target', 10, 1, this.game.scene);
     this._targetMesh.visibility = 0;
+    this._targetMesh.isPickable = false;
+
     const targetParticleSystem = new Babylon.ParticleSystem('player_target_particle', 200, this.game.scene);
     targetParticleSystem.color1 = new Color4(0.7, 0.8, 1, 1);
     targetParticleSystem.color2 = new Color4(0.2, 0.5, 1.0, 1.0);
